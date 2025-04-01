22f87191cae8a51418dac9fdcedec2b88ce7d275
tt <tt@tt.com>
2025-04-01 23:48:27 +0900

    statisticsdata

--- app/src/main/java/com/github/ttt374/healthcaretracer/ui/statics/StatisticsScree.kt
+++ app/src/main/java/com/github/ttt374/healthcaretracer/ui/statics/StatisticsScree.kt
@@ -1,21 +1,26 @@
 package com.github.ttt374.healthcaretracer.ui.statics
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.github.ttt374.healthcaretracer.data.Item
 import com.github.ttt374.healthcaretracer.data.averageOrNull
 import com.github.ttt374.healthcaretracer.data.bloodPressureFormatted
 import com.github.ttt374.healthcaretracer.data.gapME
@@ -25,43 +30,59 @@ import com.github.ttt374.healthcaretracer.ui.common.CustomTopAppBar
 import com.github.ttt374.healthcaretracer.ui.home.ItemsViewModel
 import com.github.ttt374.healthcaretracer.ui.home.toBodyWeightString
 import com.github.ttt374.healthcaretracer.ui.home.toPulseString
-import com.github.ttt374.healthcaretracer.ui.home.withSubscript
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 
 @Composable
-fun StatisticsScreen(itemsViewModel: ItemsViewModel = hiltViewModel(), appNavigator: AppNavigator) {
-    val dailyItems by itemsViewModel.dailyItems.collectAsState()
-    val items by itemsViewModel.items.collectAsState()
+fun StatisticsScreen(viewModel: StatisticsViewModel = hiltViewModel(), appNavigator: AppNavigator) {
+    val selectedRange by viewModel.selectedRange.collectAsState()
+    val statistics by viewModel.statistics.collectAsState()
+    val filteredItems by viewModel.filteredItems.collectAsState()
 
-    val bpUpperList = remember(items) { items.mapNotNull { it.bpUpper?.toDouble() } }
-    val bpLowerList = remember(items) { items.mapNotNull { it.bpLower?.toDouble()  } }
-    val pulseList = remember(items) { items.mapNotNull { it.pulse?.toDouble()  } }
-    val bodyWeightList = remember(items) { items.mapNotNull { it.bodyWeight?.toDouble()  } }
-    val meGapList = remember(dailyItems) { dailyItems.mapNotNull { it.items.gapME() } }
+    val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault())
+    val cutoffDate = Instant.now().minus(selectedRange.days, ChronoUnit.DAYS)
 
-    Scaffold(topBar = { CustomTopAppBar("Statics") },
-        bottomBar = { CustomBottomAppBar(appNavigator) }) { innerPadding ->
+    Scaffold(
+        topBar = { CustomTopAppBar("Statistics") },
+        bottomBar = { CustomBottomAppBar(appNavigator) }
+    ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
+
+            Box(modifier = Modifier.padding(16.dp)) {
+                Column {
+                    TimeRangeDropdown(selectedRange) { viewModel.setSelectedRange(it) }
+                    Text("from: " + dateFormatter.format(cutoffDate))
+                    Text("# of data: " + filteredItems.size)
+                }
+            }
+
+            HorizontalDivider(thickness = 1.5.dp, color = Color.LightGray)
             StatisticsHeadersRow()
 
             StatisticsItemRow("average",
-                bpUpperList.averageOrNull(),
-                bpLowerList.averageOrNull(),
-                meGapList.averageOrNull(),
-                pulseList.averageOrNull(),
-                bodyWeightList.averageOrNull() )
+                statistics.avgBpUpper,
+                statistics.avgBpLower,
+                statistics.avgMeGap,
+                statistics.avgPulse,
+                statistics.avgBodyWeight
+            )
             StatisticsItemRow("max",
-                bpUpperList.maxOrNull(),
-                bpLowerList.maxOrNull(),
-                meGapList.maxOrNull(),
-                pulseList.maxOrNull(),
-                bodyWeightList.maxOrNull() )     
+                statistics.maxBpUpper,
+                statistics.maxBpLower,
+                statistics.maxMeGap,
+                statistics.maxPulse,
+                statistics.maxBodyWeight
+            )
             StatisticsItemRow("min",
-                bpUpperList.minOrNull(),
-                bpLowerList.minOrNull(),
-                meGapList.minOrNull(),
-                pulseList.minOrNull(),
-                bodyWeightList.minOrNull())
+                statistics.minBpUpper,
+                statistics.minBpLower,
+                statistics.minMeGap,
+                statistics.minPulse,
+                statistics.minBodyWeight
+            )
         }
     }
 }
@@ -82,9 +103,42 @@ fun StatisticsItemRow(label: String, bpUpper: Double?, bpLower: Double?, meGap: 
     Row {
         Text(label, Modifier.weight(1f))
         Text(bloodPressureFormatted(bpUpper?.toInt(), bpLower?.toInt(), meGap?.toInt()), Modifier.weight(1f))
-        Text(pulse.toPulseString(), Modifier.weight(1f))
+        Text(pulse?.toInt().toPulseString(), Modifier.weight(1f))
         Text(bodyWeight.toBodyWeightString(), Modifier.weight(1f))
     }
     HorizontalDivider(thickness = 0.75.dp, color = Color.LightGray)
+}
+// filter
+@Composable
+fun TimeRangeDropdown(selectedRange: TimeRange, onRangeSelected: (TimeRange) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    val ranges = TimeRange.entries
+
+    //Row(modifier = Modifier.fillMaxWidth().padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
+    Box (modifier=Modifier.padding(8.dp)){
+        Button(onClick = { expanded = true }) {
+            Text(selectedRange.label)
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            ranges.forEach { range ->
+                DropdownMenuItem(text = { Text(range.label) }, onClick = {
+                    onRangeSelected(range)
+                    expanded = false
+                })
+            }
+        }
+    }
+
+}
+
+enum class TimeRange(val days: Long, val label: String) {
+    ONE_WEEK(7, "1 Week"),
+    ONE_MONTH(30, "1 Month"),
+    SIX_MONTHS(180, "6 Months")
+}
+
+fun List<Item>.filterByRange(range: TimeRange): List<Item> {
+    val cutoffDate = Instant.now().minus(range.days, ChronoUnit.DAYS)
+    return filter { it.measuredAt.isAfter(cutoffDate) }
 }
 
---
+++ app/src/main/java/com/github/ttt374/healthcaretracer/ui/statics/StatisticsViewModel.kt
@@ -0,0 +1,86 @@
+package com.github.ttt374.healthcaretracer.ui.statics
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.ttt374.healthcaretracer.data.Item
+import com.github.ttt374.healthcaretracer.data.ItemRepository
+import com.github.ttt374.healthcaretracer.data.averageOrNull
+import com.github.ttt374.healthcaretracer.data.gapME
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.temporal.ChronoUnit
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsViewModel @Inject constructor (itemRepository: ItemRepository) : ViewModel(){
+    val items = itemRepository.getAllItemsFlow().stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    private val _selectedRange = MutableStateFlow(TimeRange.ONE_WEEK)
+    val selectedRange: StateFlow<TimeRange> = _selectedRange
+
+    val filteredItems = combine(items, selectedRange) { items, range ->
+        val cutoffDate = Instant.now().minus(range.days, ChronoUnit.DAYS)
+        items.filter { it.measuredAt.isAfter(cutoffDate) }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
+
+    fun setSelectedRange(range: TimeRange) {
+        _selectedRange.value = range
+    }
+
+    val statistics = filteredItems.map { items ->
+        val bpUpperList = items.mapNotNull { it.bpUpper?.toDouble() }
+        val bpLowerList = items.mapNotNull { it.bpLower?.toDouble() }
+        val pulseList = items.mapNotNull { it.pulse?.toDouble() }
+        val bodyWeightList = items.mapNotNull { it.bodyWeight?.toDouble() }
+        val meGapList = items.groupBy { it.measuredAt.atZone(ZoneId.systemDefault()).toLocalDate() }
+            .map { (_, dailyItems) -> dailyItems.gapME() }.filterNotNull()
+
+        StatisticsData(
+            avgBpUpper = bpUpperList.averageOrNull(),
+            avgBpLower = bpLowerList.averageOrNull(),
+            avgPulse = pulseList.averageOrNull(),
+            avgBodyWeight = bodyWeightList.averageOrNull(),
+            avgMeGap = meGapList.averageOrNull(),
+            maxBpUpper = bpUpperList.maxOrNull(),
+            maxBpLower = bpLowerList.maxOrNull(),
+            maxPulse = pulseList.maxOrNull(),
+            maxBodyWeight = bodyWeightList.maxOrNull(),
+            maxMeGap = meGapList.maxOrNull(),
+            minBpUpper = bpUpperList.minOrNull(),
+            minBpLower = bpLowerList.minOrNull(),
+            minPulse = pulseList.minOrNull(),
+            minBodyWeight = bodyWeightList.minOrNull(),
+            minMeGap = meGapList.minOrNull()
+        )
+    }.stateIn(viewModelScope, SharingStarted.Lazily, StatisticsData())
+
+}
+
+data class StatisticsData(
+    val avgBpUpper: Double? = null,
+    val avgBpLower: Double? = null,
+    val avgPulse: Double? = null,
+    val avgBodyWeight: Double? = null,
+    val avgMeGap: Double? = null,
+    val maxBpUpper: Double? = null,
+    val maxBpLower: Double? = null,
+    val maxPulse: Double? = null,
+    val maxBodyWeight: Double? = null,
+    val maxMeGap: Double? = null,
+    val minBpUpper: Double? = null,
+    val minBpLower: Double? = null,
+    val minPulse: Double? = null,
+    val minBodyWeight: Double? = null,
+    val minMeGap: Double? = null
+)

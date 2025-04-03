d6c597f86f373330fd6135e5e2293a13c773da5a
tt <tt@tt.com>
2025-04-03 17:56:55 +0900

    chart target

--- app/src/main/java/com/github/ttt374/healthcaretracer/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/github/ttt374/healthcaretracer/ui/settings/SettingsScreen.kt
@@ -8,11 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.github.mikephil.charting.BuildConfig
 import com.github.ttt374.healthcaretracer.data.selectedGuideline
 import com.github.ttt374.healthcaretracer.navigation.AppNavigator
 import com.github.ttt374.healthcaretracer.ui.common.CustomBottomAppBar
 import com.github.ttt374.healthcaretracer.ui.common.CustomTopAppBar
+import com.github.ttt374.healthcaretracer.BuildConfig
 
 @Composable
 fun SettingsScreen(appNavigator: AppNavigator) {
@@ -21,7 +21,7 @@ fun SettingsScreen(appNavigator: AppNavigator) {
         bottomBar = { CustomBottomAppBar(appNavigator) }
     ) { innerPadding ->
         Column (Modifier.padding(innerPadding).padding(16.dp)){
-            SettingsRow("HTN Category Type") { Text(selectedGuideline.name) }
+            SettingsRow("HTN Guideline Type") { Text(selectedGuideline.name) }
             SettingsRow("Version") { Text(BuildConfig.VERSION_NAME) }
         }
     }

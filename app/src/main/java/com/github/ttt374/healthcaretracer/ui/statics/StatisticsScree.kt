b5007af624295823a179860c02bb229a9c86ac33
tt <tt@tt.com>
2025-04-26 20:40:15 +0900

    statrepository

--- app/src/main/java/com/github/ttt374/healthcaretracer/ui/statics/StatisticsScree.kt
+++ app/src/main/java/com/github/ttt374/healthcaretracer/ui/statics/StatisticsScree.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.github.ttt374.healthcaretracer.R

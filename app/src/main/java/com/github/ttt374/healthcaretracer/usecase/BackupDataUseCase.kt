218071e05fe58d8d3f9f14d5a2471bf102877c0e
tt <tt@tt.com>
2025-05-06 22:48:45 +0900

    csvpartial

--- app/src/main/java/com/github/ttt374/healthcaretracer/usecase/BackupDataUseCase.kt
+++ app/src/main/java/com/github/ttt374/healthcaretracer/usecase/BackupDataUseCase.kt
@@ -4,22 +4,13 @@ import android.net.Uri
 import android.util.Log
 import com.github.ttt374.healthcaretracer.data.backup.ContentResolverWrapper
 import com.github.ttt374.healthcaretracer.data.backup.CsvExporter
-import com.github.ttt374.healthcaretracer.data.backup.CsvField
 import com.github.ttt374.healthcaretracer.data.backup.CsvImporter
-import com.github.ttt374.healthcaretracer.data.backup.CsvItemPartial
-import com.github.ttt374.healthcaretracer.data.backup.toItem
-import com.github.ttt374.healthcaretracer.data.item.Item
 import com.github.ttt374.healthcaretracer.data.repository.ItemRepository
-import com.github.ttt374.healthcaretracer.shared.Logger
-import com.opencsv.CSVReader
-import com.opencsv.CSVWriter
 import jakarta.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.io.Reader
-import java.io.Writer
 
 
 /////////////////////////////////
